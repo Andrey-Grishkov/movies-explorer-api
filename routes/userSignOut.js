458804a13d3291bbd1004c58e14cfa20b9512408
Андрей Гришков <andreygorm@gmail.com>
2022-12-02 21:13:27 +0300

    clearCookie add options

--- routes/userSignOut.js
+++ routes/userSignOut.js
@@ -1,8 +1,23 @@
 const router = require('express').Router();
 const { messages } = require('../utils/messages');
 
+// router.post('/signout', (req, res) => {
+//   res.clearCookie('jwt').send({ message: messages.messageSignOut });
+// });
+
 router.post('/signout', (req, res) => {
-  res.clearCookie('jwt').send({ message: messages.messageSignOut });
+  res.clearCookie('jwt', {
+    httpOnly: true,
+      sameSite: 'none',
+      secure:true,
+  }).send({ message: messages.messageSignOut });
 });
+
+// 'jwt', token, {
+//   httpOnly: true,
+//   sameSite: 'none',
+//   secure:true,
+// }
+
 
 module.exports = router;
